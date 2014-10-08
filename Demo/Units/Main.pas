2d4d461dbecee24903ef6c2d1d16622b985961a3
b.atalay007@gmail.com <b.atalay007@gmail.com@736ebb4d-caed-9ecc-10ce-7ff8e3c534b9>
2014-10-08 11:39:43 +0000

    Added Package Download support and Demos

--- Demo/Units/Main.pas
+++ Demo/Units/Main.pas
@@ -5,27 +5,48 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  Androidapi.DownloadsManager, FMX.Layouts, FMX.Memo, FMX.Objects;
+  Androidapi.DownloadsManager, FMX.Layouts, FMX.Memo, FMX.Objects,
+  FMX.TabControl;
 
 type
   TForm3 = class(TForm)
-    Button1: TButton;
     DownloadManagers1: TDownloadManagers;
     ToolBar1: TToolBar;
     Text1: TText;
-    ProgressBar1: TProgressBar;
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    TabItem2: TTabItem;
+    Button1: TButton;
+    Button2: TButton;
     Rectangle1: TRectangle;
     Label1: TLabel;
+    ProgressBar1: TProgressBar;
     Rectangle2: TRectangle;
     Label7: TLabel;
     ProgressBar3: TProgressBar;
     Rectangle3: TRectangle;
     Label8: TLabel;
     ProgressBar2: TProgressBar;
-    Button2: TButton;
+    VertScrollBox1: TVertScrollBox;
+    DownloadManagers2: TDownloadManagers;
+    Button3: TButton;
+    TabItem3: TTabItem;
+    Button4: TButton;
+    DownloadManagers3: TDownloadManagers;
+    TabControl2: TTabControl;
+    TabItem4: TTabItem;
+    TabItem5: TTabItem;
+    Label2: TLabel;
+    VertScrollBox2: TVertScrollBox;
+    DownloadManagers4: TDownloadManagers;
     procedure Button1Click(Sender: TObject);
-    procedure DownloadManagers1Progress(Sender: TObject; Items: TDownloadItems);
     procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure DownloadManagers1Progress(Sender: TObject; Items: TDownloadItems;
+      Package: TPackage);
+    procedure DownloadManagers3Progress(Sender: TObject; Items: TDownloadItems;
+      Package: TPackage);
   private
     { Private declarations }
   public
@@ -73,8 +94,92 @@ begin
   DownloadManagers1.Items.StopDownload(5);
 end;
 
+procedure TForm3.Button3Click(Sender: TObject);
+begin
+  A := DownloadManagers2;
+
+  A.ClearAll;
+
+  with A.Items.Add do
+  begin
+    Source := 'http://live.sysinternals.com/ADExplorer.exe';
+    Target := System.IOUtils.TPath.GetDocumentsPath + PathDelim + 'ADExplorer1.exe';
+  end;
+
+  with A.Items.Add do
+  begin
+    Source := 'http://live.sysinternals.com/ADExplorer.exe';
+    Target := System.IOUtils.TPath.GetDocumentsPath + PathDelim + 'ADExplorer2.exe';
+  end;
+
+  with A.Items.Add do
+  begin
+    Source := 'http://live.sysinternals.com/ADExplorer.exe';
+    Target := System.IOUtils.TPath.GetDocumentsPath + PathDelim + 'ADExplorer3.exe';
+  end;
+
+  A.StartAll;
+end;
+
+procedure TForm3.Button4Click(Sender: TObject);
+begin
+  case TabControl2.TabIndex of
+    0:
+    begin
+      A := DownloadManagers3;
+      with A.PackageDownload.Add do
+      begin
+        with DownloadsLink do
+        begin
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0016.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0017.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0018.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0019.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0020.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0021.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0022.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0023.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0024.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0025.jpg');
+        end;
+        Target := System.IOUtils.TPath.GetDocumentsPath;
+        Start;
+      end;
+    end;
+    1:
+    begin
+      A := DownloadManagers4;
+      A.ParentVertScroll := VertScrollBox2;
+
+      with A.PackageDownload.Add do
+      begin
+        with DownloadsLink do
+        begin
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0001.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0002.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0003.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0004.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0005.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0006.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0007.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0008.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0009.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0010.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0011.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0012.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0013.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0014.jpg');
+          Add('http://mangakafe.com/Mangalar/Noragami/05/0015.jpg');
+        end;
+        Target := System.IOUtils.TPath.GetDocumentsPath;
+        Start;
+      end;
+    end;
+  end;
+end;
+
 procedure TForm3.DownloadManagers1Progress(Sender: TObject;
-  Items: TDownloadItems);
+  Items: TDownloadItems; Package: TPackage);
 var
   I: Integer;
 begin
@@ -95,6 +200,23 @@ begin
       end;
     end;
   end;
+
+end;
+
+procedure TForm3.DownloadManagers3Progress(Sender: TObject;
+  Items: TDownloadItems; Package: TPackage);
+var
+  I: Integer;
+begin
+  for I := 0 to DownloadManagers3.PackageDownload.Count - 1 Do
+  begin
+    case I of
+      0: begin
+        Label2.Text := Format('Ýndirilen 1:  %s/%s',[Package.Item[i].Value.ToString,Package.Item[i].Count.ToString]);
+      end;
+    end;
+  end;
+
 end;
 
 end.
--- Install/Units/Androidapi.DownloadsManager.pas
+++ Install/Units/Androidapi.DownloadsManager.pas
@@ -1,12 +1,17 @@
 unit Androidapi.DownloadsManager;
 
 interface
-Uses System.Classes, System.Actions, System.SysUtils,
+Uses System.Classes, System.Actions, System.SysUtils, FMX.StdCtrls,
      Generics.Collections,
-     IdHttp, IdComponent;
+     IdHttp, IdComponent,
+     FMX.Layouts, FMX.Objects, FMX.Types, FMX.Forms;
 
 type
   TDownloadManagers = class;
+  TPackage = class;
+  TDownloadItems = class;
+
+  TOnProgress = procedure(Sender: TObject; Items: TDownloadItems; Package: TPackage) of object;
 
   TDownloadItems = class(TObject)
     {$REGION 'Item Class'}
@@ -14,15 +19,23 @@ type
       TDownloadItem = class
       private
         Http            : TIdHTTP;
+        RList           : TList<TRectangle>;
+        TTList          : TList<TText>;
+        PList          : TList<TProgressBar>;
         FSource,
         FTarget         : String;
         FPercent        : Integer;
-        FOwner : TDownloadItems;
+        FOwner          : TDownloadItems;
+        BG              : TRectangle;
+        xPercent,
+        FTitle          : TText;
+        FProgress       : TProgressBar;
         function SGetWrite: String;
         function TGetWrite: String;
         procedure SSetWrite(const Value: String);
         procedure TSetWrite(const Value: String);
         procedure HttpWork (ASender: TObject; AWorkMode: TWorkMode; AWorkCount: Int64);
+        procedure AddDownloadItem(AOwner:TVertScrollBox);
       public
         constructor Create(AOwner: TDownloadItems);
         destructor  Destroy; override;
@@ -41,14 +54,76 @@ type
       FOwner : TDownloadManagers;
       function GetItem(const Index: Integer): TDownloadItem;
     public
-      procedure StopDownload(const Index: Integer);
+      procedure   StopDownload(const Index: Integer);
       constructor Create(AOwner: TDownloadManagers);
       destructor  Destroy; override;
-      function Add: TDownloadItem;
-      property Item[const Index: Integer]     : TDownloadItem read GetItem; default;
+      function    Add: TDownloadItem;
+      property    Item[const Index: Integer]     : TDownloadItem  read GetItem; default;
   end;
 
-  TOnProgress = procedure(Sender: TObject; Items: TDownloadItems) of object;
+  TPackageItem = class
+    Const Seperators = '/';
+    {$REGION 'Thread'}
+    private type
+     TPackageThread = class(TThread)
+      private
+        Http   : TIdHTTP;
+        FIndex : Integer;
+        FOwner :TPackage;
+      public
+        constructor Create(TItem: TPackage;TIndex: Integer);
+        destructor Destroy; override;
+        procedure Execute; override;
+      end;
+      {$ENDREGION}
+    private
+      RList       : TList<TRectangle>;
+      TTList      : TList<TText>;
+      PList       : TList<TProgressBar>;
+      FLink       : TStringList;
+      FTarget     : String;
+      FIndex      : Integer;
+      FOwner      : TPackage;
+      FCount,
+      FValue      : Integer;
+      BG          : TRectangle;
+      xPercent,
+      FTitle      : TText;
+      FProgress   : TProgressBar;
+      Islem       : TPackageThread;
+      procedure AddDownloadItem(AOwner:TVertScrollBox);
+      function GetName(T: String): String;
+      function TGetWrite: String;
+      procedure TSetWrite(const Value: String);
+      function GetTitle: String;
+      procedure SetTitle(const Value: String);
+    public
+      constructor Create(AOwner: TPackage);
+      destructor  Destroy; override;
+      procedure   Start(Index: Integer = - 1 );
+      property    Target        : String      read TGetWrite write TSetWrite;
+      property    Count         : Integer     read FCount;
+      property    Value         : Integer     read FValue;
+      property    DownloadsLink : TStringList read FLink     write FLink;
+      property    Title         : String      read GetTitle  write SetTitle;
+  end;
+
+  TPackage = class(TPersistent)
+    private
+      FOwner      : TDownloadManagers;
+      FItems      : TObjectList<TPackageItem>;
+      FName       : TList<String>;
+      function GetItem(const Index: Integer): TPackageItem;
+    public
+      constructor Create(AOwner: TComponent);
+      destructor  Destroy; override;
+      procedure   Assign(Source : TPersistent); override;
+      procedure   StartAll;
+      function    Add: TPackageItem;
+      function    Count: Integer;
+      property    Item[const Index: Integer] : TPackageItem read GetItem; default;
+    published
+  end;
 
   TDownloadManagers = class(TComponent)
   {$REGION 'Thread'}
@@ -67,7 +142,11 @@ type
     private
       Islem: TDownloadThread;
       FDownloadItems: TDownloadItems;
+      FParent : TVertScrollBox;
+      FPackage: TPackage;
       function GetCount: Integer;
+      function GetParent: TVertScrollBox;
+      procedure SetParent(const Value: TVertScrollBox);
     protected
       FOnProgress: TOnProgress;
     public
@@ -76,12 +155,12 @@ type
       procedure StartAll;
       procedure ClearAll;
       procedure StopAll;
-      procedure DoChange(T:TDownloadItems);
-
-      property Count          : Integer        read GetCount;
-      property Items          : TDownloadItems read FDownloadItems;
+      property Count           : Integer        read GetCount;
+      property Items           : TDownloadItems read FDownloadItems;
+      property PackageDownload : TPackage   read FPackage;
     published
-      property OnProgress : TOnProgress        read FOnProgress write FOnProgress;
+      property ParentVertScroll    : TVertScrollBox read GetParent   write SetParent;
+      property OnProgress : TOnProgress             read FOnProgress write FOnProgress;
   end;
 
 procedure Register;
@@ -96,27 +175,56 @@ end;
 { TDownloadManagers }
 
 procedure TDownloadManagers.ClearAll;
+var I, J, C: Integer;
 begin
   if FDownloadItems.FItems.Count > 0 then
+  begin
+    C := FDownloadItems.FItems.Count;
+    if FParent <> nil then
+    begin
+      for I := 0 to C -1 do
+      begin
+        for J := Items.Item[I].PList.Count -1 Downto 0  do
+          Items.Item[I].PList.Items[J].Free;
+
+        for J := Items.Item[I].TTList.Count -1 Downto 0 do
+          Items.Item[I].TTList.Items[J].Free;
+
+        for J := Items.Item[I].RList.Count -1 Downto 0 do
+          Items.Item[I].RList.Items[J].Free;
+      end;
+    end;
     FDownloadItems.FItems.Clear;
+  end;
 end;
 
 constructor TDownloadManagers.Create(AOwner: TComponent);
 begin
   inherited;
   FDownloadItems := TDownloadItems.Create(Self);
-//  FOnProgress    := nil;
+  FPackage       := TPackage.Create(Self);
 end;
 
 destructor TDownloadManagers.Destroy;
 begin
   FDownloadItems.Free;
+  FPackage.Free;
   inherited;
 end;
 
 function TDownloadManagers.GetCount: Integer;
 begin
   Result := FDownloadItems.FItems.Count;
+end;
+
+function TDownloadManagers.GetParent: TVertScrollBox;
+begin
+  Result := FParent;
+end;
+
+procedure TDownloadManagers.SetParent(const Value: TVertScrollBox);
+begin
+  FParent := Value;
 end;
 
 procedure TDownloadManagers.StartAll;
@@ -138,7 +246,6 @@ begin
   if FDownloadItems.FItems.Count > 0 then
     for I := 0 to GetCount -1  do
       Items[i].Stop;
-//      Items.
 end;
 
 { TDownloadItems }
@@ -169,27 +276,102 @@ begin
   inherited;
 end;
 
-procedure TDownloadManagers.DoChange(T:TDownloadItems);
-begin
-//  if Assigned(TDownloadManagers(FOwner).FOnProgress) then
-//    TDownloadManagers(FOwner).OnProgress(Self);
-  if Assigned(FOnProgress) then FOnProgress(Self,T);
-end;
-
 function TDownloadItems.GetItem(const Index: Integer): TDownloadItem;
 begin
   Result := FItems[Index];
 end;
 
+procedure TDownloadItems.TDownloadItem.AddDownloadItem(AOwner: TVertScrollBox);
+begin
+  BG := TRectangle.Create(AOwner);
+  RList.Add(BG);
+  with BG do
+  begin
+    Parent     := AOwner;
+    Fill.Color := $FFF39912;
+    Align      := TAlignLayout.Top;
+    Height     := 46;
+    with Margins do
+    begin
+      Top   := 10;
+      Left  := 5;
+      Right := 5;
+    end;
+    Sides := [];
+
+    FTitle := TText.Create(BG);
+    TTList.Add(FTitle);
+    with FTitle do
+    begin
+      Parent := BG;
+      Align  := TAlignLayout.Top;
+      Height := 25;
+      Color := $FF34495E;
+      with TextSettings do
+      begin
+        Font.Size := 16;
+        HorzAlign := TTextAlign.Leading;
+      end;
+      Text := 'Download Item';
+    end;
+
+    FProgress := TProgressBar.Create(BG);
+    PList.Add(FProgress);
+    with FProgress do
+    begin
+      Parent      := BG;
+      with Margins do
+      begin
+        Left  := 5;
+        Right := 5;
+      end;
+      Align       := TAlignLayout.Client;
+      Min         := 0;
+      Max         := 100;
+    end;
+
+    xPercent := TText.Create(BG);
+    TTList.Add(xPercent);
+    with xPercent do
+    begin
+      Parent := BG;
+      Align  := TAlignLayout.Bottom;
+      Height := 15;
+      with TextSettings do
+        Font.Size := 12;
+      Color := $FF34495E;
+      Text := '%0';
+    end;
+  end;
+end;
+
 constructor TDownloadItems.TDownloadItem.Create(AOwner: TDownloadItems);
 begin
   FOwner := AOwner;
+  if FOwner.FOwner.FParent <> nil then
+  begin
+    RList  := TList<TRectangle>.Create;
+    TTList := TList<TText>.Create;
+    PList := TList<TProgressBar>.Create;
+    AddDownloadItem(FOwner.FOwner.FParent);
+  end;
 end;
 
 destructor TDownloadItems.TDownloadItem.Destroy;
 begin
+  RList.Clear;
+  TTList.Clear;
+  PList.Clear;
+  RList.Free;
+  TTList.Free;
+  PList.Free;
 
   inherited;
+end;
+
+function TPackageItem.GetTitle: String;
+begin
+  Result := FTitle.Text;
 end;
 
 procedure TDownloadItems.TDownloadItem.HttpWork(ASender: TObject; AWorkMode: TWorkMode;
@@ -204,11 +386,23 @@ begin
   if (Pos('chunked', LowerCase(Http.Response.TransferEncoding)) = 0) and
      (ContentLength > 0) then
   begin
-    FPercent := 100 * AWorkCount div ContentLength;
+    FPercent        := 100 * AWorkCount div ContentLength;
+    if FOwner.FOwner.ParentVertScroll <> nil then
+    begin
+      FProgress.Value := FPercent;
+      xPercent.Text   := '%' + FPercent.ToString;
+    end;
     if Assigned(FOwner) and Assigned(FOwner.FOwner.OnProgress) then
-         FOwner.FOwner.OnProgress(Self, FOwner);
+         FOwner.FOwner.OnProgress(Self, FOwner,nil);
     // Tekiklenme yeri % ler hesaplanýyor.
   end;
+end;
+
+procedure TPackageItem.SetTitle(const Value: String);
+begin
+  if Assigned(FTitle) then
+    if Value <> FTitle.Text then
+      FTitle.Text := Value;
 end;
 
 function TDownloadItems.TDownloadItem.SGetWrite: String;
@@ -272,7 +466,6 @@ end;
 
 destructor TDownloadManagers.TDownloadThread.Destroy;
 begin
-
   inherited;
 end;
 
@@ -280,6 +473,240 @@ procedure TDownloadManagers.TDownloadThread.Execute;
 begin
   inherited;
   FItems.FItems.Items[FIndex].Start;
+end;
+
+{ TPackageItem }
+
+function TPackage.Add: TPackageItem;
+begin
+  Result := TPackageItem.Create(Self);
+  FItems.Add(Result);
+end;
+
+procedure TPackageItem.AddDownloadItem(AOwner: TVertScrollBox);
+begin
+  BG := TRectangle.Create(AOwner);
+  RList.Add(BG);
+  with BG do
+  begin
+    Parent     := AOwner;
+    Fill.Color := $FFF39912;
+    Align      := TAlignLayout.Top;
+    Height     := 46;
+    with Margins do
+    begin
+      Top   := 10;
+      Left  := 5;
+      Right := 5;
+    end;
+    Sides := [];
+
+    FTitle := TText.Create(BG);
+    TTList.Add(FTitle);
+    with FTitle do
+    begin
+      Parent := BG;
+      Align  := TAlignLayout.Top;
+      Height := 25;
+      Color := $FF34495E;
+      with TextSettings do
+      begin
+        Font.Size := 16;
+        HorzAlign := TTextAlign.Leading;
+      end;
+      Text := 'Download Item';
+    end;
+
+    FProgress := TProgressBar.Create(BG);
+    PList.Add(FProgress);
+    with FProgress do
+    begin
+      Parent      := BG;
+      with Margins do
+      begin
+        Left  := 5;
+        Right := 5;
+      end;
+      Align       := TAlignLayout.Client;
+      Min         := 0;
+      Max         := 100;
+    end;
+
+    xPercent := TText.Create(BG);
+    TTList.Add(xPercent);
+    with xPercent do
+    begin
+      Parent := BG;
+      Align  := TAlignLayout.Bottom;
+      Height := 15;
+      with TextSettings do
+        Font.Size := 12;
+      Color := $FF34495E;
+      Text := '%0';
+    end;
+  end;
+end;
+
+procedure TPackage.Assign(Source: TPersistent);
+begin
+end;
+
+function TPackage.Count: Integer;
+begin
+  Result := FItems.Count;
+end;
+
+constructor TPackage.Create(AOwner: TComponent);
+begin
+  FOwner := AOwner as TDownloadManagers;
+  FItems := TObjectList<TPackageItem>.Create;
+  FName  := TList<String>.Create;
+end;
+
+destructor TPackage.Destroy;
+begin
+  FItems.Free;
+  FName.Free;
+  inherited;
+end;
+
+function TPackage.GetItem(const Index: Integer): TPackageItem;
+begin
+  Result := FItems[Index];
+end;
+
+procedure TPackage.StartAll;
+var
+  I: Integer;
+begin
+  for I := 0 to Count -1  do
+    FItems[I].Start(I);
+end;
+
+{ TPackageItem.TPackageThread }
+
+constructor TPackageItem.TPackageThread.Create(TItem: TPackage;
+  TIndex: Integer);
+begin
+  inherited Create(True);
+  FOwner := TItem;
+  FIndex := TIndex;
+end;
+
+destructor TPackageItem.TPackageThread.Destroy;
+begin
+  inherited;
+end;
+
+procedure TPackageItem.TPackageThread.Execute;
+var
+  MS: TMemoryStream;
+  TmpName,
+  Tmp : String;
+  Key,
+  xCount,
+  I   : Integer;
+  Temp: TPackageItem;
+begin
+  inherited;
+  Temp := FOwner.FItems[FIndex];
+  xCount := Temp.FLink.Count;
+  Temp.FCount := xCount;
+  Key    := 1;
+  for I := 0 TO xCount - 1 do
+  begin
+    TmpName := Temp.GetName(Temp.FLink[i]);
+
+    Http := TIdHTTP.Create(nil);
+    With Http do
+    begin
+      ReadTimeout := 5000;
+      try
+        MS := TMemoryStream.Create;
+
+        Get(Temp.FLink[i], MS);
+
+        if Temp.FTarget[Temp.FTarget.Length] <> PathDelim then
+          Tmp := Temp.FTarget + PathDelim
+        else
+          Tmp := Temp.FTarget;
+
+        Temp.SetTitle(Copy(TmpName, 1, Pos('.',TmpName) - 1));
+
+        MS.SaveToFile(Tmp + TmpName);
+
+        Temp.FValue := Key;
+
+        if Assigned(Temp.xPercent) then
+          Temp.xPercent.Text := Key.ToString + '/' + xCount.ToString;
+
+        if Assigned(FOwner.FOwner.OnProgress) then
+          FOwner.FOwner.OnProgress(Self, nil,FOwner);
+
+        Inc(Key)
+      finally
+        Disconnect; Free;
+        MS.Free;
+      end;
+    end;
+  end;
+end;
+
+{ TPackageItem }
+
+constructor TPackageItem.Create(AOwner: TPackage);
+begin
+  FLink  := TStringList.Create;
+  FOwner := AOwner;
+  if Assigned(FOwner.FOwner.FParent) then
+  begin
+    RList  := TList<TRectangle>.Create;
+    TTList := TList<TText>.Create;
+    PList := TList<TProgressBar>.Create;
+    AddDownloadItem(FOwner.FOwner.FParent);
+  end;
+end;
+
+destructor TPackageItem.Destroy;
+begin
+  RList.Free;
+  TTList.Free;
+  PList.Free;
+  FLink.Free;
+  inherited;
+end;
+
+function TPackageItem.GetName(T: String): String;
+begin
+  if T.Trim = '' then Exit;
+  while Pos(Seperators,T) > 0 do
+  System.Delete(T,1,Pos(Seperators,T));
+  Result := T;
+end;
+
+procedure TPackageItem.Start(Index: Integer = - 1 );
+var
+  Key: Integer;
+begin
+  if Index = -1 then
+    Key := FOwner.FItems.Count - 1
+  else
+    Key := Index;
+
+  Islem := TPackageThread.Create(FOwner,Key);
+  Islem.FreeOnTerminate := True;
+  Islem.Start;
+end;
+
+function TPackageItem.TGetWrite: String;
+begin
+  Result := FTarget;
+end;
+
+procedure TPackageItem.TSetWrite(const Value: String);
+begin
+  if Value <> FTarget then
+    FTarget := Value;
 end;
 
 end.
